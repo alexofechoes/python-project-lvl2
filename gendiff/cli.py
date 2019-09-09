6cf294f1114639f878e426cf6d249be4a0beb112
Alexander Bukharin <alexawm182@gmail.com>
2019-09-09 10:50:49 +0300

    stage-2: add arg --format

--- gendiff/cli.py
+++ gendiff/cli.py
@@ -5,7 +5,7 @@ import argparse
 def main():
     """Run cli."""
     parser = argparse.ArgumentParser(description='Generate diff')
-    parser.add_argument('-f', dest='FORMAT')
+    parser.add_argument('-f', '--format', dest='FORMAT', help='set format of output')
     parser.add_argument('first_file', metavar='first_file')
     parser.add_argument('second_file', metavar='second_file')
 
