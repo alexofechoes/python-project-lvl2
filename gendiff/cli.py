5101f61ddb0fe6506a3f408238c9f9fe5bdc39e0
Alexander Bukharin <alexawm182@gmail.com>
2019-09-09 09:42:46 +0300

    add cli help

---
+++ gendiff/cli.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*-
+import argparse
+
+
+def main():
+    """Run cli."""
+    parser = argparse.ArgumentParser(description='Generate diff')
+    parser.add_argument('-f', dest='FORMAT')
+    parser.add_argument('first_file', metavar='first_file')
+    parser.add_argument('second_file', metavar='second_file')
+
+    parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
